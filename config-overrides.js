1cf6f7468e38ecb9da98a06b8173eb6c28e7bd1b
kielkow <matheuskiel@fiorifer.com.br>
2020-01-01 18:28:18 -0300

    Config enviroment

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { addBabelPlugin, override } = require('customize-cra');
+
+module.exports = override(
+  addBabelPlugin([
+    'babel-plugin-root-import',
+    {
+      rootPathSuffix: 'src',
+    },
+  ])
+);
