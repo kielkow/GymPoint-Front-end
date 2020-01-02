dd0c6d17f7f372a7ed1443af29490e0e8b5071a3
kielkow <matheuskiel@fiorifer.com.br>
2020-01-02 12:06:07 -0300

    Config Reactotron

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+import Reactotron from 'reactotron-react-js';
+
+if (process.env.NODE_ENV === 'development') {
+  const tron = Reactotron.configure().connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
