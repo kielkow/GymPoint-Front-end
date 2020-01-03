298c5357e9dd3d685af380d6417e351a7538b96f
kielkow <matheuskiel@fiorifer.com.br>
2020-01-03 20:49:43 -0300

    Authentication persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-console */
+import { persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
+
 import createStore from './createStore';
+import persistReducers from './persistReducers';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
@@ -14,8 +17,9 @@ const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const middlewares = [sagaMiddleware];
 
-const store = createStore(rootReducer, middlewares);
+const store = createStore(persistReducers(rootReducer), middlewares);
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistor };
