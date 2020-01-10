846bcb4a65599cf86acd442c911be6e5c3a3bc9a
kielkow <matheuskiel@fiorifer.com.br>
2020-01-09 21:33:31 -0300

    Modal Help Orders

--- src/pages/HelpOrders/index.js
+++ src/pages/HelpOrders/index.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
 import { Container, Content } from './styles';
 
+const useStyles = makeStyles(theme => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    maxWidth: '500px',
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: theme.palette.background.paper,
+    border: '0',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(3, 3, 3),
+    borderRadius: '4px',
+  },
+}));
+
 export default function Students() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Container>
       <header>
@@ -17,7 +50,7 @@ export default function Students() {
           <li>
             <span>Matheus Kielkowski</span>
             <div>
-              <button id="answer" type="button">
+              <button id="answer" type="button" onClick={handleOpen}>
                 answer
               </button>
             </div>
@@ -56,6 +89,77 @@ export default function Students() {
           </li>
         </ul>
       </Content>
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <div>
+              <span style={{ fontWeight: 'bold' }}>QUESTION STUDENT</span>
+              <p
+                style={{
+                  marginTop: '10px',
+                  color: '#796b6bee',
+                  wordSpacing: '5px',
+                }}
+              >
+                It has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was
+                popularised in the 1960s with the release of Letraset sheets
+                containing Lorem?
+              </p>
+            </div>
+            <div
+              style={{
+                marginTop: '20px',
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              <span style={{ fontWeight: 'bold' }}>ANSWER</span>
+              <textarea
+                placeholder="Your answer..."
+                style={{
+                  fontSize: '14px',
+                  padding: '5px 0px',
+                  borderRadius: '4px',
+                  border: '0',
+                  margin: '10px 0px',
+                  flex: '1',
+                  resize: 'none',
+                  overflow: 'hidden',
+                }}
+              />
+            </div>
+            <button
+              type="button"
+              style={{
+                background: '#ee4d63',
+                color: '#fff',
+                padding: '10px 10px',
+                border: '0',
+                fontWeight: 'bold',
+                borderRadius: '4px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              Answer Student
+            </button>
+          </div>
+        </Fade>
+      </Modal>
     </Container>
   );
 }
