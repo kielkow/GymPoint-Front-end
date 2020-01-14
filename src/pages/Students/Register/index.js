928040b920366bed527ca7315f71bdf87536a718
kielkow <matheuskiel@fiorifer.com.br>
2020-01-14 13:47:20 -0300

    Save Student

--- src/pages/Students/Register/index.js
+++ src/pages/Students/Register/index.js
@@ -1,12 +1,100 @@
-import React from 'react';
+/* eslint-disable consistent-return */
+import React, { useState } from 'react';
 import { Input } from '@rocketseat/unform';
 import { MdArrowBack, MdSave } from 'react-icons/md';
 
 import { Link } from 'react-router-dom';
 
+import { toast } from 'react-toastify';
+import history from '~/services/history';
 import { Container, Content } from './styles';
 
+import api from '~/services/api';
+
 export default function RegisterStudent() {
+  const [student, setStudent] = useState({
+    provider: true,
+    name: '',
+    email: '',
+    age: '',
+    weigth: '',
+    heigth: '',
+  });
+
+  async function saveStudent() {
+    const arrayStudent = Object.values(student);
+    let isNull = false;
+    arrayStudent.forEach(propStudent => {
+      if (propStudent === null || propStudent === '') isNull = true;
+    });
+
+    if (isNull) return toast.error('Please check student´s information');
+
+    try {
+      await api.post('/students', student);
+      toast.success('Student created with success!');
+      history.push('/students');
+    } catch (err) {
+      toast.error('This student already exists!');
+    }
+  }
+
+  function handleChangeName(e) {
+    const newStudent = {
+      provider: true,
+      name: e.target.value,
+      email: student.email,
+      age: student.age,
+      weigth: student.weigth,
+      heigth: student.heigth,
+    };
+    setStudent(newStudent);
+  }
+  function handleChangeEmail(e) {
+    const newStudent = {
+      provider: true,
+      name: student.name,
+      email: e.target.value,
+      age: student.age,
+      weigth: student.weigth,
+      heigth: student.heigth,
+    };
+    setStudent(newStudent);
+  }
+  function handleChangeAge(e) {
+    const newStudent = {
+      provider: true,
+      name: student.name,
+      email: student.email,
+      age: Number(e.target.value),
+      weigth: student.weigth,
+      heigth: student.heigth,
+    };
+    setStudent(newStudent);
+  }
+  function handleChangeWeigth(e) {
+    const newStudent = {
+      provider: true,
+      name: student.name,
+      email: student.email,
+      age: student.age,
+      weigth: Number(e.target.value),
+      heigth: student.heigth,
+    };
+    setStudent(newStudent);
+  }
+  function handleChangeHeigth(e) {
+    const newStudent = {
+      provider: true,
+      name: student.name,
+      email: student.email,
+      age: student.age,
+      weigth: student.weigth,
+      heigth: Number(e.target.value),
+    };
+    setStudent(newStudent);
+  }
+
   return (
     <Container>
       <header>
@@ -16,7 +104,7 @@ export default function RegisterStudent() {
             <MdArrowBack color="#fff" size={18} />
             <span>Back</span>
           </Link>
-          <button type="button">
+          <button type="button" onClick={saveStudent}>
             <MdSave color="#fff" size={18} />
             <span>Save</span>
           </button>
@@ -25,16 +113,30 @@ export default function RegisterStudent() {
       <Content type="submit">
         <div>
           <span>FULL NAME</span>
-          <Input name="name" placeholder="Student name..." />
+          <Input
+            name="name"
+            placeholder="Student name..."
+            onChange={handleChangeName}
+          />
         </div>
         <div>
           <span>E-MAIL</span>
-          <Input name="email" type="email" placeholder="Student e-mail..." />
+          <Input
+            name="email"
+            type="email"
+            placeholder="Student e-mail..."
+            onChange={handleChangeEmail}
+          />
         </div>
         <div className="paternDiv">
           <div className="childDiv">
             <span>AGE</span>
-            <Input name="age" type="number" placeholder="Student age..." />
+            <Input
+              name="age"
+              type="number"
+              placeholder="Student age..."
+              onChange={handleChangeAge}
+            />
           </div>
           <div className="childDiv">
             <span>WEIGTH (kg)</span>
@@ -42,6 +144,7 @@ export default function RegisterStudent() {
               name="weigth"
               type="number"
               placeholder="Student weigth..."
+              onChange={handleChangeWeigth}
             />
           </div>
           <div className="childDiv">
@@ -50,6 +153,7 @@ export default function RegisterStudent() {
               name="heigth"
               type="number"
               placeholder="Student heigth..."
+              onChange={handleChangeHeigth}
             />
           </div>
         </div>
