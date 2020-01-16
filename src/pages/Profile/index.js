6a86f1c3e08bbc1a0da57a9dc6d3ef3ebe4f3618
kielkow <matheuskiel@fiorifer.com.br>
2020-01-16 20:49:48 -0300

    Sign out button

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
+import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import AvatarInput from './AvatarInput';
@@ -16,6 +17,11 @@ export default function Profile() {
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data));
   }
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
@@ -37,7 +43,9 @@ export default function Profile() {
         <button type="submit">Update Profile</button>
       </Form>
 
-      <button type="button">Sign out</button>
+      <button type="button" onClick={handleSignOut}>
+        Sign out
+      </button>
     </Container>
   );
 }
