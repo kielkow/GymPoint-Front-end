62d962fafddff2fb584b0cade79b90e3b6b5be43
kielkow <matheuskiel@fiorifer.com.br>
2020-01-16 20:19:21 -0300

    Profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full name..." />
+        <Input name="email" type="email" placeholder="Your e-mail..." />
+
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Your password" />
+        <Input type="password" name="password" placeholder="New password..." />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm password..."
+        />
+
+        <button type="submit">Update Profile</button>
+      </Form>
+
+      <button type="button">Sign out</button>
+    </Container>
+  );
 }
