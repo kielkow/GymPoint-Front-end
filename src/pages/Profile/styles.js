7af032ddf9f4b9ea4bcfb0bfe5e0264c918e4e4b
kielkow <matheuskiel@fiorifer.com.br>
2020-01-16 21:08:49 -0300

    Change style inputs on profile

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -9,16 +9,11 @@ export const Container = styled.div`
     flex-direction: column;
     margin-top: 30px;
     input {
-      background: rgba(0, 0, 0, 0.1);
-      border: 0;
+      border: 0.5px solid #eee;
       border-radius: 4px;
       height: 44px;
       padding: 0 15px;
-      color: #fff;
       margin: 0 0 10px;
-      &::placeholder {
-        color: rgba(255, 255, 255, 0.7);
-      }
     }
     span {
       color: #fb6f91;
